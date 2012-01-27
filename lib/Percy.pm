e07c583db34ab37308c52bd189c70f4791d51b3e
Pedro Melo <melo@simplicidade.org>
2012-01-27 15:18:07 +0000

    Get rid of Percy methods, its an empty class now, to be discontinued
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Percy.pm
+++ lib/Percy.pm
@@ -4,22 +4,5 @@ package Percy;
 # VERSION
 # AUTHORITY
 
-use Percy::Object;
-use Percy::Storage;
-use JSON::XS qw( encode_json decode_json );
-use namespace::clean;
-
-
-## FIXME: connection stufff should be moved to Storage, eventually
-has 'dbh' => (is => 'ro');
-
-
-## Serialization
-sub encode_to_db   { return encode_json($_[1]) }
-sub decode_from_db { return decode_json($_[1]) }
-
-
-## Storage operations
-sub deploy { return Percy::Storage->deploy(@_) }
 
 1;
