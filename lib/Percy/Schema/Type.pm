ed2498e513def92cf477c404f71694df39365952
Pedro Melo <melo@simplicidade.org>
2012-01-28 13:18:26 +0000

    Make sure all default's are loaded into each ::Type instance
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Percy/Schema/Type.pm
+++ lib/Percy/Schema/Type.pm
@@ -5,6 +5,16 @@ use Percy::Utils qw( generate_uuid );
 use JSON::XS qw( encode_json decode_json );
 use namespace::clean -except => 'new';
 
+## Setup type defaults - we basically force all attributes to their
+## defaults values
+sub BUILD {
+  my ($self) = @_;
+
+  $self->$_() for qw(generate_id_cb encode_to_db_cb decode_from_db_cb);
+}
+
+
+## Basic object attributes
 has 'type' => (is => 'ro', required => 1);
 
 
@@ -16,7 +26,7 @@ has 'generate_id_cb' => (
   },
 );
 
-sub generate_id { return $_[0]->generate_id_cb->(@_) }
+sub generate_id { return $_[0]{generate_id_cb}(@_) }
 
 
 ## Encode/decode data to/from DB
@@ -34,8 +44,8 @@ has 'decode_from_db_cb' => (
   },
 );
 
-sub encode_to_db   { return $_[0]->encode_to_db_cb->(@_) }
-sub decode_from_db { return $_[0]->decode_from_db_cb->(@_) }
+sub encode_to_db   { return $_[0]{encode_to_db_cb}(@_) }
+sub decode_from_db { return $_[0]{decode_from_db_cb}(@_) }
 
 
 1;
